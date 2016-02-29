693bdddcc1003b964904205d51143d21d9da712e
Olivier Serve <tifauv@gmail.com>
2016-02-29 18:57:47 +0100

    Compilation using gcc for Release-type builds now uses march=native.
    Added support to build the project uwing clang. Just use the build-clang.sh script instead if build.sh.

---
+++ build-clang.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+srcdir=$(readlink --canonicalize $(dirname $0) )
+builddir=$(readlink --canonicalize "${srcdir}/build-clang" )
+
+echo ">> Sources in ${srcdir}"
+echo ">> Build   in ${builddir}"
+
+# Create the build directory if needed
+if [ ! -d ${builddir} ]
+then
+	mkdir ${builddir}
+fi
+
+# Move into build directory
+cd ${builddir}
+
+# Set the CXX environment variable
+export CXX=/usr/bin/clang++
+
+# Generate the Makefiles
+cmake $@ -DUSE_CLANG=on -D_CMAKE_TOOLCHAIN_PREFIX=llvm- ${srcdir}
+
+# Build
+make -j5
