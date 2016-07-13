037341bb18959fa3830794499e61c8030f1bf068
Olivier Serve <tifauv@gmail.com>
2016-07-13 17:08:56 +0200

    [history]  History: fixed the format of the include guard.

--- src/core/History.h
+++ src/core/History.h
@@ -14,8 +14,8 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
-#ifndef History_h
-#define History_h
+#ifndef HISTORY_H
+#define HISTORY_H
 
 #include <stack>
 #include "Literal.h"
