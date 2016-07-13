a16fd0c5392ad491650e5fcfc4e4347b5cd888ba
Olivier Serve <tifauv@gmail.com>
2016-07-13 17:12:38 +0200

    [heuristics]  LeastUsedPolaritySelector: fixed the format of the include guard.

--- src/heuristics/LeastUsedPolaritySelector.h
+++ src/heuristics/LeastUsedPolaritySelector.h
@@ -14,8 +14,8 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
-#ifndef LeastUsedPolaritySelector_h
-#define LeastUsedPolaritySelector_h
+#ifndef LEAST_USED_POLARITY_SELECTOR_H
+#define LEAST_USED_POLARITY_SELECTOR_H
 
 #include "PolaritySelector.h"
 
