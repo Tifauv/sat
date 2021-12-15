737cf657cda47812a0dc8642b02d6be24c28c0d8
Olivier Serve <tifauv@gmail.com>
2021-12-15 16:52:21 +0100

    Sudoku solving script
    Fixed the analysis of the valuation in case the first variable is
    negative.

--- sudoku/sudoku.sh
+++ sudoku/sudoku.sh
@@ -21,12 +21,18 @@ then
 fi
 
 # Génération du problème
+>&2 echo "# Generating problem file..."
 problem_file=$(mktemp)
 ./gen_sudoku_constraints.py > $problem_file
+>&2 echo "  - general sudoku constraints generated."
 sed -n -i -e "/^%$/r ${initial_grid}" -e 1x -e '2,${x;p}' -e '${x;p}' "$problem_file"
+>&2 echo "  - specific problem data added."
 
 # Résolution et formatage du résultat
-../build/src/solveSat "$problem_file" | grep -e '^v ' | sed -e 's/^v \(.*\) 0$/\1/' | sed -e 's/ -[0-9]*//g' | tr ' ' '\n' | sort -k 1.2,1.3 | sed -e 's/^\([0-9]\).*$/\1/' | xargs -L9 echo > "${initial_grid}.solution"
+>&2 echo
+>&2 echo "# Solving..."
+../build/src/solveSat "$problem_file" | grep -e '^v ' | sed -e 's/^v \(.*\) 0$/\1/' | sed -E 's/(^-| -)[0-9]*//g' | tr ' ' '\n' | sort -k 1.2,1.3 | sed -e 's/^\([0-9]\).*$/\1/' | xargs -L9 echo > "${initial_grid}.solution"
+>&2 echo "  - finished."
 
 # Nettoyage du fichier temporaire
 rm "$problem_file"
